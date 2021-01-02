a945a712569d1b10d4cf18ef8b2e3505d639f9d1
Linu09 <38865727+Linu09@users.noreply.github.com>
2021-01-02 13:26:20 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+#1. Create a greeting for your program.
+print("Welcome to the  Name Generator.")
+print("\n")
+#2. Ask the user for the city that they grew up in.
+street = input("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What's your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + street + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
